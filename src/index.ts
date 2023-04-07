2e890ed4bd88397a97ce71346a439bc460276b91
Stefan Schwarz <fxbrain@gmail.com>
2023-04-07 21:32:55 +0000

    ts: add initial code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello');
+/* eslint-disable no-console */
+import { GitCommand } from './git_commands.js';
+
+const git = new GitCommand('.');
+git
+  .run('status', [])
+  .then((output) => {
+    console.log(output);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+const commitCount = await git.run('rev-list', ['--count', 'HEAD', '^main']);
+console.log('Total commits: ' + commitCount);
